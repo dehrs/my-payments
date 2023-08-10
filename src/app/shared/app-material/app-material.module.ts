8cef7e11ac3af204ad2705260588b9c42eb20b82
dehrs <deborarsousa@gmail.com>
2023-08-10 11:27:18 -0300

    feat: btn filter

--- src/app/shared/app-material/app-material.module.ts
+++ src/app/shared/app-material/app-material.module.ts
@@ -4,10 +4,17 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { MatTooltipModule } from '@angular/material/tooltip';
 import { DialogModule } from '@angular/cdk/dialog';
+import { CdkMenuModule } from '@angular/cdk/menu';
 
 @NgModule({
   declarations: [],
   imports: [CommonModule],
-  exports: [MatIconModule, MatButtonModule, MatTooltipModule, DialogModule],
+  exports: [
+    MatIconModule,
+    MatButtonModule,
+    MatTooltipModule,
+    DialogModule,
+    CdkMenuModule,
+  ],
 })
 export class AppMaterialModule {}
